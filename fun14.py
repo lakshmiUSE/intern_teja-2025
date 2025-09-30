7c9511528f9147eb27d801aa2268a2403c3d2f8c
tejaswini322 <tejapernapati@gmail.com>
2025-09-30 10:27:06 -0400

    Update fun14.py
    Signed-off-by: tejaswini322 <tejapernapati@gmail.com>

--- fun14.py
+++ fun14.py
@@ -3,9 +3,10 @@
 def student_report(m1,m2,m3,m4,m5,m6):
   total=m1+m2+m3+m4+m5+m6
   avg=total/6
-  return(m1,m2,m3,m4,m4,m5,m6,total,avg)
+  return(m1,m2,m3,m4,m5,m6,total,avg)
 marks=student_report(70,50,80,90,95,85)
 print("marks:",marks[0:6])
 print("total:",marks[6])
 print("average:",marks[7])
+
 
