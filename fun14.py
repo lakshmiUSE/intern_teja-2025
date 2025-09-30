c36df45d3e102a93981dd297ea838f3b68f48c12
tejaswini322 <tejapernapati@gmail.com>
2025-09-30 10:11:32 -0400

    Update fun14.py
    Signed-off-by: tejaswini322 <tejapernapati@gmail.com>

--- fun14.py
+++ fun14.py
@@ -1,10 +1,11 @@
 #WAF we can return multiple values from the function
 #ex:students marks,sum and avrg
-def student_report(m1,m2,m3,m4,m4,m5,m6):
+def student_report(m1,m2,m3,m4,m5,m6):
   total=m1+m2+m3+m4+m5+m6
   avg=total/6
   return(m1,m2,m3,m4,m4,m5,m6,total,avg)
 marks=student_report(70,50,80,90,95,85)
 print("marks:",marks[0:6])
 print("total:",marks[6])
-print("average:",marks[7]
+print("average:",marks[7])
+
